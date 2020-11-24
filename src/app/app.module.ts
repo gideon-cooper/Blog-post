08b121fb5a9b3b2c60c748cbbb3ec82ebe8da711
Gideon Cooper <Gideon.Cooper@hotmail.com>
2020-11-24 17:28:17 +1300

    adding posts nearly finished

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,38 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FormsModule} from "@angular/forms";
+import {MatFormFieldModule} from "@angular/material/form-field"
+import {MatInputModule} from '@angular/material/input'
+import {MatCardModule} from "@angular/material/card"
+import {MatButtonModule} from "@angular/material/button"
+import {MatToolbarModule} from "@angular/material/toolbar"
+import {MatExpansionModule} from "@angular/material/expansion"
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { PostCreateComponent} from "./posts/post-create/post-create.component"
+import { PostCreateComponent} from "./posts/post-create/post-create.component";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './header/header.component';
+import { PostListComponent } from './posts/post-list/post-list.component'
 
 @NgModule({
   declarations: [
     AppComponent,
-    PostCreateComponent
+    PostCreateComponent,
+    HeaderComponent,
+    PostListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    MatInputModule,
+    MatCardModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatToolbarModule,
+    MatExpansionModule,
+    AppRoutingModule,
+    FormsModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -3,8 +3,14 @@ import { Component } from '@angular/core';
 @Component({
     templateUrl: "./post-create.component.html",
     selector: "app-post-create",
+    styleUrls: ["./post-create.component.css"]
 
 })
 export class PostCreateComponent {
-
+    enteredValue='';
+    newPost = '';
+    onAddPost(){
+        
+       this.newPost = this.enteredValue
+    }
 }
