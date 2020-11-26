ffe82e7371372e71380b8cdfd774fdd56653cae7
Gideon Cooper <Gideon.Cooper@hotmail.com>
2020-11-26 17:00:34 +1300

    hid env file

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const postsRoutes = require('./routes/posts')
 const path = require('path')
-require('dotenv').config({ path: path.join(__dirname, '.env') })
+require('dotenv').config({ path: path.join(__dirname, '/../.env') })
 const mongoose = require("mongoose")
 
 const server = express()
